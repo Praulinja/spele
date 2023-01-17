c4ba571288f1a94b61418f4ab6eb9ffa969cc2fa
Praulinja <beta.pasts@gmail.com>
2023-01-17 08:22:56 +0200

    uztaisiju speliti

---
+++ funkcijas.js
@@ -0,0 +1,37 @@
+let datoraSkaitlis = 10;
+
+function nolasitIevadito(){
+    //atrod input lauka saturu
+    let ievadits = document.getElementById ("guess").value
+    console.log(ievadits);
+    return  ievadits;
+}
+
+function salidzinaSkaitlus(cilvekaSkaitlis){
+    if (isNaN(cilvekaSkaitlis))
+    return "Noteikumus izlasi"
+if(cilvekaSkaitlis>datoraSkaitlis){
+    return "datora skaitlis ir mazāks"
+};
+if(cilvekaSkaitlis<datoraSkaitlis){
+    return "datora skaitlis ir lielāks"
+};
+return "atbilde ir pareiza"
+}
+
+function izvaditAtbildi(atbilde){
+document.getElementById("atbilde").innerHTML=atbilde;
+}
+
+function veiktParbaudi(){
+    console.log("Pārbaudīts!!!", datoraSkaitlis);
+    let minejums = nolasitIevadito();
+    let rezultats = salidzinaSkaitlus(minejums);
+    izvaditAtbildi(rezultats);
+}
+
+function nomainitSkaitli(){
+    datoraSkaitlis = Math.round(Math.random() * 100)
+    console.log(datoraSkaitlis);
+    izvaditAtbildi("Jauna spēle")
+}
